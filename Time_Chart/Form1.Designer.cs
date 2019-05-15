11575397c3201f0cda21f06aca0167006d7b6744
diana <diatuaeva@gmail.com>
2019-05-15 09:56:22 +0300

    Добавьте файлы проекта.

---
+++ Time_Chart/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Time_Chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelTimeStart = new System.Windows.Forms.Label();
+            this.labelTimeEnd = new System.Windows.Forms.Label();
+            this.labelTimeDelta = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(46, 41);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(316, 52);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "запустить  функцию";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // labelTimeStart
+            // 
+            this.labelTimeStart.AutoSize = true;
+            this.labelTimeStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTimeStart.Location = new System.Drawing.Point(57, 156);
+            this.labelTimeStart.Name = "labelTimeStart";
+            this.labelTimeStart.Size = new System.Drawing.Size(86, 31);
+            this.labelTimeStart.TabIndex = 1;
+            this.labelTimeStart.Text = "label1";
+            // 
+            // labelTimeEnd
+            // 
+            this.labelTimeEnd.AutoSize = true;
+            this.labelTimeEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTimeEnd.Location = new System.Drawing.Point(59, 261);
+            this.labelTimeEnd.Name = "labelTimeEnd";
+            this.labelTimeEnd.Size = new System.Drawing.Size(86, 31);
+            this.labelTimeEnd.TabIndex = 2;
+            this.labelTimeEnd.Text = "label2";
+            // 
+            // labelTimeDelta
+            // 
+            this.labelTimeDelta.AutoSize = true;
+            this.labelTimeDelta.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTimeDelta.Location = new System.Drawing.Point(59, 357);
+            this.labelTimeDelta.Name = "labelTimeDelta";
+            this.labelTimeDelta.Size = new System.Drawing.Size(86, 31);
+            this.labelTimeDelta.TabIndex = 3;
+            this.labelTimeDelta.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(57, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Время старта";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(59, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(258, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Время завершения";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(59, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(263, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Время затраченное";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelTimeDelta);
+            this.Controls.Add(this.labelTimeEnd);
+            this.Controls.Add(this.labelTimeStart);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelTimeStart;
+        private System.Windows.Forms.Label labelTimeEnd;
+        private System.Windows.Forms.Label labelTimeDelta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
