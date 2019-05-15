144a61515e2ff9077f8b529741ba7045ec77c31f
diana <diatuaeva@gmail.com>
2019-05-15 12:26:25 +0300

    два графика

--- Time_Chart/Form1.Designer.cs
+++ Time_Chart/Form1.Designer.cs
@@ -31,6 +31,7 @@
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.buttonStart = new System.Windows.Forms.Button();
             this.labelTimeStart = new System.Windows.Forms.Label();
             this.labelTimeEnd = new System.Windows.Forms.Label();
@@ -40,6 +41,9 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.labelParam = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonStart
@@ -123,7 +127,6 @@
             this.label4.Size = new System.Drawing.Size(278, 31);
             this.label4.TabIndex = 8;
             this.label4.Text = "Значение параметра";
-            
             // 
             // labelParam
             // 
@@ -134,13 +137,46 @@
             this.labelParam.Size = new System.Drawing.Size(59, 31);
             this.labelParam.TabIndex = 7;
             this.labelParam.Text = "100";
-            
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(398, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(316, 52);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "запустить  функцию";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(407, 194);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(801, 300);
+            this.chart1.TabIndex = 10;
+            this.chart1.Text = "chart1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1258, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.labelParam);
             this.Controls.Add(this.label3);
@@ -152,7 +188,7 @@
             this.Controls.Add(this.buttonStart);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.chartFunction)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,6 +205,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label labelParam;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- Time_Chart/Form1.cs
+++ Time_Chart/Form1.cs
@@ -26,7 +26,7 @@ namespace Time_Chart
         int countDivides(int a)
         {
             int count = 0;
-            for (int i = 1; i < a; i++)
+            for (int i = 1; i < Math.Sqrt(a); i++)
                 if (a % i == 0) count++;
             return count;
 
@@ -54,13 +54,38 @@ namespace Time_Chart
 
         private void buttonChart_Click(object sender, EventArgs e)
         {
-            chartFunction.Series[0].Points.AddXY(10, 10);
-            chartFunction.Series[0].Points.AddXY(100, 20);
-            chartFunction.Series[0].Points.AddXY(200, 100);
+           // chartFunction.Series[0].Points.AddXY(10, 10);
+         //   chartFunction.Series[0].Points.AddXY(100, 20);
+//chartFunction.Series[0].Points.AddXY(200, 100);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("stat.txt");
+            StreamReader sr1 = new StreamReader("stat1.txt");
+           
+            while (!sr.EndOfStream)
+            {
+                string[] s = sr.ReadLine().Split();
+                string ss = s[0].Split(':')[2];
+                double t = Convert.ToDouble(ss.Replace('.', ','));
+                int n = Convert.ToInt32(s[1]);
+                chart1.Series[0].Points.AddXY(n, t);
+
+                s = sr1.ReadLine().Split();
+                ss = s[0].Split(':')[2];
+                t = Convert.ToDouble(ss.Replace('.', ','));
+                n = Convert.ToInt32(s[1]);
+                chart1.Series[1].Points.AddXY(n, t);
+
+            }
+            sr.Close();
+            sr1.Close();
+            }
         }
     }
 
       
 
    
-    }
+
